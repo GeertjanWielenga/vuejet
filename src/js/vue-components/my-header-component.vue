db47552da2a82e5bd3bb01de27f968892beb0625
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2017-07-07 14:16:44 +0200

    adding menu to header

--- src/js/vue-components/my-header-component.vue
+++ src/js/vue-components/my-header-component.vue
@@ -1,15 +1,35 @@
 <template>
     <div>
-        <span role="img" class="oj-sm-only-hide oj-icon demo-oracle-icon" title="Oracle Logo" alt="Oracle Logo"></span>
-        <span class="oj-web-applayout-header-title">{{name}}</span>
+        <div>
+            <span role="img" class="oj-sm-only-hide oj-icon demo-oracle-icon" title="Oracle Logo" alt="Oracle Logo"></span>
+            <span class="oj-web-applayout-header-title">{{name}}</span>
+        </div>
+        <div class="oj-flex-bar-end">
+            <button id="userMenu"/>
+            <ul id='menu1' >
+                <li id="pref"><a href="#">Preferences</a></li>
+                <li id="help"><a href="#">Help</a></li>
+                <li id="about"><a href="#">About</a></li>
+                <li id="out"><a href="#">Sign Out</a></li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
-    define(['Vue'], function (Vue) {
+    define(['Vue', 'ojs/ojbutton', 'ojs/ojmenu', 'ojs/ojtoolbar'], function (Vue) {
       Vue.component('my-header-component', {
-          template: template,
-          props: ['name']
+        template: template,
+        props: ['name'],
+        mounted() {
+            $('#userMenu').ojButton({
+                menu: '#menu1',
+                label: "john.hancock@oracle.com",
+                chroming: 'half'
+            });
+            $('#menu1').ojMenu({
+            });
+        }
       });
     });
 </script>
