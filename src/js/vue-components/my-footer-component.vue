879922620723cf362c91b8024e93c20879c2e7ba
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2017-07-07 13:23:39 +0200

    adding links to footer section

--- src/js/vue-components/my-footer-component.vue
+++ src/js/vue-components/my-footer-component.vue
@@ -2,9 +2,9 @@
     <div>
         <div class="oj-web-applayout-footer-item oj-web-applayout-max-width">
             <ul>
-              <li>aaa</li>
-              <li>bbb</li>
-              <li>ccc</li>
+                <li v-for='link in links'>
+                    <a :href='link.url' target="_blank">{{link.title}}</a>
+                </li>
             </ul>
          </div>
         <div class="oj-web-applayout-footer-item oj-web-applayout-max-width oj-text-secondary-color oj-text-sm">
@@ -16,7 +16,29 @@
 <script>
     define(['Vue'], function (Vue) {
       Vue.component('my-footer-component', {
-          template: template
+          template: template,
+          data: function() {
+            return {
+                links: [
+                  {
+                  'url': 'http://www.oracle.com/us/corporate/index.html#menu-about',
+                  'title': 'About Oracle'
+                },{
+                  'url': 'http://www.oracle.com/us/corporate/contact/index.html',
+                  'title': 'Contact Us'
+                },{
+                  'url': 'http://www.oracle.com/us/legal/index.html',
+                  'title': 'Legal Notices'
+                },{
+                  'url': 'http://www.oracle.com/us/legal/terms/index.html',
+                  'title': 'Terms Of Use'
+                },{
+                  'url': 'http://www.oracle.com/us/legal/privacy/index.html',
+                  'title': 'Your Privacy Rights'
+                }
+              ]
+            };
+          }
       });
     });
 </script>
