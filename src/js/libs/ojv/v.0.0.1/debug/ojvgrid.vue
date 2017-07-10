64617eb1f1067252ee119518cfd1ccc40950abf9
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2017-07-10 17:42:32 +0200

    ojvgrid

---
+++ src/js/libs/ojv/v.0.0.1/debug/ojvgrid.vue
@@ -0,0 +1,79 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th v-for="key in columns"
+                    @click="sortBy(key)"
+                    :class="{ active: sortKey == key }">
+                    {{ key | capitalize }}
+                    <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
+                    </span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="entry in filteredData">
+                <td v-for="key in columns">
+                    {{entry[key]}}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+	define(["Vue"], function (Vue) {
+	  Vue.component("ojvgrid", {
+		template: template, // the variable template will be injected
+		props: {
+		  data: Array,
+		  columns: Array,
+		  filterKey: String
+		},
+		data: function () {
+		  var sortOrders = {}
+		  this.columns.forEach(function (key) {
+			sortOrders[key] = 1
+		  })
+		  return {
+			sortKey: '',
+			sortOrders: sortOrders
+		  }
+		},
+		computed: {
+		  filteredData: function () {
+			var sortKey = this.sortKey
+			var filterKey = this.filterKey && this.filterKey.toLowerCase()
+			var order = this.sortOrders[sortKey] || 1
+			var data = this.data
+			if (filterKey) {
+			  data = data.filter(function (row) {
+				return Object.keys(row).some(function (key) {
+				  return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+				})
+			  })
+			}
+			if (sortKey) {
+			  data = data.slice().sort(function (a, b) {
+				a = a[sortKey]
+				b = b[sortKey]
+				return (a === b ? 0 : a > b ? 1 : -1) * order
+			  })
+			}
+			return data
+		  }
+		},
+		filters: {
+		  capitalize: function (str) {
+			return str.charAt(0).toUpperCase() + str.slice(1)
+		  }
+		},
+		methods: {
+		  sortBy: function (key) {
+			this.sortKey = key
+			this.sortOrders[key] = this.sortOrders[key] * -1
+		  }
+		}
+	  });
+	});
+</script>
--- src/js/viewModels/customers_1.js
+++ src/js/viewModels/customers_1.js
@@ -1,10 +1,31 @@
-define(['Vue'], function(Vue){
+define(['Vue','vue!libs/ojv/v.0.0.1/debug/ojvgrid'], function(Vue) {
   return Vue.extend({
-    template: `
-      <div class="oj-hybrid-padding">
-	    <h3>Customers Content Area</h3>
-        To change the content of this section, you will make edits to the customers.html file located in the /js/views folder.
-      </div>
-    `
+   template: `
+    <div class="oj-hybrid-padding">
+		<section id="grid">
+		  <form id="search">
+			  Search <input name="query" v-model="searchQuery">
+		  </form>
+		  <br/>
+		  <ojvgrid
+			  :data="gridData"
+			  :columns="gridColumns"
+			  :filter-key="searchQuery">
+		  </ojvgrid>
+	  </section>
+    </div>
+    `,
+    data: function() {
+      return {
+		  searchQuery: '',
+		  gridColumns: ['name', 'power'],
+		  gridData: [
+			{name: 'Chuck Norris', power: Infinity},
+			{name: 'Bruce Lee', power: 9000},
+			{name: 'Jackie Chan', power: 7000},
+			{name: 'Jet Li', power: 8000}
+		  ]
+      };
+    }
   });
 });
