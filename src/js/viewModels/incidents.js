2320ad8f05979a0e103ca290778a309d74bc58f0
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2017-07-10 23:40:38 +0200

    configurable chart

--- src/js/viewModels/incidents.js
+++ src/js/viewModels/incidents.js
@@ -4,7 +4,8 @@ define(['Vue', 'text!views/incidents.html', 'vue!libs/ojv/v.0.0.1/debug/ojvchart
 			template: file,
 			data: function () {
 			  return {
-				stats: [{name: "Series 1", items: [42, 34]},
+				stats: [
+				  {name: "Series 1", items: [42, 34]},
 				  {name: "Series 2", items: [55, 30]},
 				  {name: "Series 3", items: [36, 50]},
 				  {name: "Series 4", items: [22, 46]},
