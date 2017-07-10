e60984b5bd85cc2a15ec2f093eb033f18561c3c5
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2017-07-10 23:01:06 +0200

    configurable chart

--- src/js/libs/ojv/v.0.0.1/debug/ojvchart.vue
+++ src/js/libs/ojv/v.0.0.1/debug/ojvchart.vue
@@ -9,18 +9,15 @@
           template: template,
           props: {
 	    type: String,
-	    orientation: String
+	    orientation: String,
+	    series: Array
           },
           mounted() {
             $(this.$el).ojChart({
                 type: this.type,
                 orientation: this.orientation,
                 stack: 'off',
-                series: [ {name: "Series 1", items: [42, 34]},
-                             {name: "Series 2", items: [55, 30]},
-                             {name: "Series 3", items: [36, 50]},
-                             {name: "Series 4", items: [22, 46]},
-                             {name: "Series 5", items: [22, 46]}],
+                series: this.series,
                 groups: ["Group A", "Group B"],
                 animationOnDisplay: 'auto',
                 animationOnDataChange: 'auto',
--- src/js/viewModels/incidents.js
+++ src/js/viewModels/incidents.js
@@ -1,6 +1,15 @@
-define(['Vue', 'text!views/incidents.html', 'vue!libs/ojv/v.0.0.1/debug/ojvchart'], 
- function(Vue, file){
-     return Vue.extend({
-       template: file
-     });
-});
+define(['Vue', 'text!views/incidents.html', 'vue!libs/ojv/v.0.0.1/debug/ojvchart'],
+		function (Vue, file) {
+		  return Vue.extend({
+			template: file,
+			data: function () {
+			  return {
+				stats: [{name: "Series 1", items: [42, 34]},
+				  {name: "Series 2", items: [55, 30]},
+				  {name: "Series 3", items: [36, 50]},
+				  {name: "Series 4", items: [22, 46]},
+				  {name: "Series 5", items: [22, 46]}]
+			  };
+			}
+		  });
+		});
