57192b9662f3e31567915e557f1dd79f1caffd09
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2017-07-10 20:49:41 +0200

    ojvchart

---
+++ src/js/libs/ojv/v.0.0.1/debug/ojvchart.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="chart"></div>
+</template>
+
+<script>
+   define(['Vue', 'ojs/ojchart'],
+    function (Vue) {
+      Vue.component('ojvchart', {
+          template: template,
+          mounted() {
+            $('#chart').ojChart({
+                type: 'bar',
+                orientation: 'vertical',
+                stack: 'off',
+                series: [ {name: "Series 1", items: [42, 34]},
+                             {name: "Series 2", items: [55, 30]},
+                             {name: "Series 3", items: [36, 50]},
+                             {name: "Series 4", items: [22, 46]},
+                             {name: "Series 5", items: [22, 46]}],
+                groups: ["Group A", "Group B"],
+                animationOnDisplay: 'auto',
+                animationOnDataChange: 'auto',
+                hoverBehavior: 'dim'
+            });
+          }
+      });
+    });
+</script>
