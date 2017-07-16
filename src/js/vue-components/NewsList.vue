a5b8898eedf6000b1f6eff7d150c0e3354a09c49
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2017-07-16 07:26:26 +0200

    integrate with ojFilmstrip

--- src/js/vue-components/NewsList.vue
+++ src/js/vue-components/NewsList.vue
@@ -1,48 +1,55 @@
 <template>
     <div class="container">
-      <ul class="media-list">
-        <li class="media" v-for="article in articles">
-          <div class="media-left">
-            <a v-bind:href="article.url" target="_blank">
-              <img class="media-object" v-bind:src="article.urlToImage">
-            </a>
-          </div>
-          <div class="media-body">
-            <h4 class="media-heading"><a v-bind:href="article.url" target="_blank">{{article.title}}</a></h4>
-            <h5><i>by {{article.author}}</i></h5>
-            <p>{{article.description}}</p>
-          </div>
-        </li>
-      </ul>
+        <div id="filmStrip">
+            <span class="oj-panel oj-panel-alt2 demo-filmstrip-item" v-for="article in articles">
+                <div class="media-body">
+                    <h4 class="media-heading"><a v-bind:href="article.url" target="_blank">{{article.title}}</a></h4>
+                    <h5><i>by {{article.author}}</i></h5>
+                    <p>{{article.description}}</p>
+                    <div class="media-left">
+                        <a v-bind:href="article.url" target="_blank">
+                            <img class="media-object" v-bind:src="article.urlToImage">
+                        </a>
+                    </div>
+                </div>
+            </span>
+        </div>
     </div>
 </template>
 
 <script>
-	define(['Vue'], function (Vue) {
-	  Vue.component('NewsList', {
-		template: template,
-		props: ['source'],
-		data() {
-		  return {
-			articles: []
-		  }
-		},
-		methods: {
-		  updateSource: function (source) {
-			this.$http.get('https://newsapi.org/v1/articles?source=' + source + '&apiKey=28c1e583f45f4b9681096130ab063229')
-					.then(response => {
-					  this.articles = response.data.articles;
-					});
-		  }
-		},
-		created: function () {
-		  this.updateSource(this.source);
-		},
-		watch: {
-		  source: function (val) {
-			this.updateSource(val);
-		  }
-		}
-	  });
-	});
+        define(['Vue', 'ojs/ojfilmstrip'], function (Vue) {
+          Vue.component('NewsList', {
+                template: template,
+                props: ['source'],
+                data() {
+                  return {
+                        articles: []
+                  };
+                },
+                methods: {
+                  updateSource: function (source) {
+                        this.$http.get('https://newsapi.org/v1/articles?source=' + source + '&apiKey=28c1e583f45f4b9681096130ab063229')
+                                        .then(response => {
+                                          this.articles = response.data.articles;
+                                        });
+                        $('#filmStrip').ojFilmStrip("refresh");
+                  }
+                },
+                created: function () {
+                  this.updateSource(this.source);
+                },
+                mounted() {
+                  $('#filmStrip').ojFilmStrip({
+                        arrowPlacement: "adjacent",
+                        arrowVisibility: "visible"
+                  });
+                },
+                watch: {
+                  source: function (val) {
+                        this.updateSource(val);
+                  }
+                }
+          });
+        });
 </script>
--- src/js/vue-components/SourceSelection.vue
+++ src/js/vue-components/SourceSelection.vue
@@ -26,7 +26,6 @@
           },
           methods: {
             sourceChanged: function(e) {
-             alert('hello2');
              for (var i=0; i<this.sources.length; i++) {
                if (this.sources[i].id == e.target.value) {
                  this.source = this.sources[i];
